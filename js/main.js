6cf65fccc6deca3ebecc2c6bbd71550b8c089e97
Virginie Chaix <chaixvirginie883@gmail.com>
2020-10-20 13:50:21 +0200

    mis a jour

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import {getAllTeddyBear} from './utilities.js';
 import {createCard} from './utilities.js';
+/*
 import {getOneTeddyBear} from './utilities.js';
-
+*/
 getAllTeddyBear().then(function(data){createCard(data)});
 
 
--- js/utilities.js
+++ js/utilities.js
@@ -1,9 +1,15 @@
 const url = "http://localhost:3000/api/teddies";
 
-
 let urlJson= new URL(window.location);
 let id= urlJson.searchParams.get("id");
 
+function ObjectSize(obj) {
+    let size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 
 export function getAllTeddyBear() {
     return new Promise((resolve, reject) => {
@@ -12,17 +18,19 @@ export function getAllTeddyBear() {
             .then(function(response){ resolve(response)})
             .catch(reject);
     })
-   
-}
+   }
 
 export function createCard(data){
-    for (let i=0; i < 4 ;i++){
-      document.getElementById("cardContainer").insertAdjacentHTML('beforeend', '<div class="card col-6 p-1 mb-2" style="width: 18rem;"> <img src=" '+ data[i].imageUrl + '" class="card-img-top" alt="..."> <div class="card-body">  <h5 class="card-title">'+ data[i].name  + '</h5> <p class="card-text">' + data[i].price + ' euros</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div>'
+    for (let i=0; i <ObjectSize(data); i++){   
+      let price = parseInt(data[i].price);
+      price = price / 100;
+      price = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price); 
+      document.getElementById("cardContainer").insertAdjacentHTML('beforeend', '<div class="card col-6 p-1 mb-2" style="width: 18rem;"> <img src=" '+ data[i].imageUrl + '" class="card-img-top" alt="..."> <div class="card-body">  <h5 class="card-title">'+ data[i].name  + '</h5> <p class="card-text">' + price + ' euros</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div>'
       )}
 }
 
 
-function getOneTeddyBear(url,id){
+export function getOneTeddyBear(url,id){
     return new Promise((resolve, reject) => {
         fetch(url+id)
             .then(response => response.json()) 
@@ -32,7 +40,7 @@ function getOneTeddyBear(url,id){
 
 }
 
-function displayOneTB (){
+export function displayOneTB (data, id){
+    document.getElementById("teddy").insertAdjacentHTML('beforeend', '<div class="col-2"><img src="" alt=""></div>');
 
-    
 }
