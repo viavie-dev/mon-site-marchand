391fe07e29b5d3c5a432d366c574df00c1e4bb07
Virginie Chaix <chaixvirginie883@gmail.com>
2020-10-21 08:34:24 +0200

    mise a jour fiche product

---
+++ js/panier.js
@@ -0,0 +1,5 @@
+export function getProducts() {
+    return new Promise((resolve, reject) => {
+        
+    });
+}
--- js/utilities.js
+++ js/utilities.js
@@ -22,7 +22,7 @@ export function createCard(data){
       let price = parseInt(data[i].price);
       price = price / 100;
       price = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price); 
-      document.getElementById("cardContainer").insertAdjacentHTML('beforeend', '<div class="card col-6 p-1 mb-2" style="width: 18rem;"> <img src=" '+ data[i].imageUrl + '" class="card-img-top" alt="..."> <div class="card-body">  <h5 class="card-title">'+ data[i].name  + '</h5> <p class="card-text">' + price + ' euros</p> <a href="teddyBear.html?id='+data[i]._id +'" class="btn btn-primary">En savoir plus</a> </div>'
+      document.getElementById("cardContainer").insertAdjacentHTML('beforeend', '<div class="card col-6 p-1 mb-2" style="width: 18rem;"> <img src=" '+ data[i].imageUrl + '" class="card-img-top" style="height: 400px"alt="..."> <div class="card-body">  <h5 class="card-title">'+ data[i].name  + '</h5> <p class="card-text">' + price + ' euros</p><a href="teddyBear.html?id='+data[i]._id +'" class="btn btn-primary mr-2">En savoir plus</a><a href="#" class="btn btn-primary">Panier</a></div>'
       )}
 }
 
@@ -43,6 +43,10 @@ export function displayOneTB (product){
     let price = parseInt(product.price);
     price = price / 100;
     price = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price); 
-    document.getElementById("teddy").insertAdjacentHTML('beforeend', '<div class="card col-6"><img src="'+ product.imageUrl + ' " alt="nounours" style="width: 100%;"></div><div class="card col-6"><h1>'+product.name+'</h1><h3>Couleurs : '+product.colors+'</h3><h2>'+price+'</h2><p>'+product.description+'</p></div>');
+
+    let matchesColors= [{'rgb(152,118,84)': "Pale brown", '':"white"}];
+    document.getElementById("teddy").insertAdjacentHTML('beforeend', '<div class="card col-6"><img src="'+ product.imageUrl + ' " alt="nounours" style="width: 100%;"></div><div class="card col-6"><h1>'+product.name+'</h1><h3>Couleurs : '+product.colors+'</h3><h2>'+price+'</h2><p>'+product.description+'</p><form action="" method="GET"><select name="quantity" id="quantity"><option value="1">1</option><option value="1">2</option><option value="1">3</option><option value="1">4</option><option value="1">5</option><option value="1">6</option><option value="1">7</option><option value="1">8</option><option value="1">9</option><option value="1">10</option></select></form><a href="panier.html" class="btn btn-primary" style="width: 140px">Panier</a></div>');
 
 }
+
+
